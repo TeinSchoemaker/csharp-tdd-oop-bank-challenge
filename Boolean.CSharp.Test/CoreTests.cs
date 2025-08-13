00a58aa3bf8feb60f23e965651660ed7b8c96470
Tein Schoemaker <teintjomme@gmail.com>
2025-08-13 12:24:00 +0200

    Update CoreTests.cs

--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -15,10 +15,53 @@ namespace Boolean.CSharp.Test
         }
 
         [Test]
-        public void TestQuestion1()
+        public void CreateAccountCreatesNewUserAccount()
         {
+            string userName = "Tjommert"
+            string password = "12345"
 
+            database.AddUser(Account.CreateAccount(userName, password));
+            
+            var total = checkout.TotalPrice(basket);
+
+            Assert.IsTrue(database.ContainsKey("Tjommert")
         }
 
+        [Test]
+        public void CreateSavingsAddedSavingsToAccount()
+        {
+            var user1 = new User("Tjommert", "12345);
+
+            user1.CreateSavings("savings1");
+        
+            Assert.Contains(user1, savings.Id);
+        }
+
+        [Test]
+        public void GenerateStatementHasDatesAmountsAndBalance()
+        {
+            var user1 = new User("Tjommert", "12345);
+
+            var savings = user1.CreateSavings("savings1");
+            var statement = GenerateStatement(user1.savings[1]);                    
+        
+            Assert.Contains(statement, $"Date");
+            Assert.Contains(statement, $"Credit");
+            Assert.Contains(statement, $"Debit");
+            Assert.Contains(statement, $"Balance");                                          
+        }
+
+        [Test]
+        public void DepositAndWithdrawFundsFromAccount()
+        {
+            var user1 = new User("Tjommert", "12345);
+            var savings = user1.CreateSavings("savings1");
+
+            savings.Deposit(4000.00);
+            Assert.That(savings.Balance, Is.EqualTo(4000.00));
+
+            savings.Withdraw(2500.00);
+            Assert.That(savings.Balance, Is.EqualTo(1500.00));
+        }
     }
 }
